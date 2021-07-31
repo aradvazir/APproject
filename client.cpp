aeccda4bc99863f98ec4abe61909c49503cb03e7
aradvazir <aradvazir@gmail.com>
2021-07-31 11:40:48 +0430

    fourth commit

--- client.cpp
+++ client.cpp
@@ -4,20 +4,28 @@ using namespace std;
 using namespace httplib;
 
 int main(void) {
-    Client clientObject1("localhost", 8080);
+    Client clientObject("localhost", 8080);
     while (true)
     {
+        int endgame=0;
         while(true)
         {
             sleep(1);
-            if (!clientObject1.Get("print"))
+            if (!clientObject.Get("print"))
             {}
             else
             {
-                if ( auto result = clientObject1.Get("print")) {
+                if ( auto result = clientObject.Get("print")) {
                     if ( result->status == 200 ) {
                         string s = result->body;
                         cout << s << endl;
+                        if (s[125]!='0')    //125 is 126th element in string s. which is x[5][5]
+                        {
+                            cout << "player " << s[125] << " won!" << endl;
+                            endgame=1;
+                            clientObject.Get("stop");
+                            break;
+                        }
                     }
                     else
                     {
@@ -30,6 +38,10 @@ int main(void) {
                 break;
             }
         }
+        if (endgame)
+        { // it means one of the players has reached the x[5][5]
+            break;
+        }
         int C;
         cout << "1.place Wall\n 2.move" << endl;
         cin >> C;
@@ -41,7 +53,7 @@ int main(void) {
             ofstream index_file("index.txt", ios:: binary);
             index_file << p << ' ' << q << ' ' << r;
             index_file.close();
-            clientObject1.Get("wall");
+            clientObject.Get("wall");
         }
         else if (C==2)
         {
@@ -52,13 +64,19 @@ int main(void) {
                 cout << "try again" << endl;
                 break;
             }
-            if (auto result = clientObject1.Get(B.data())) {
+            if (auto result = clientObject.Get(B.data())) {
                 if ( result->status == 200 ) {
                     string s = result->body;
                     cout << s << endl;
+                    if (s[125]!='0')
+                    {
+                        cout << "player " << s[125] << " won!" << endl;
+                        clientObject.Get("stop");
+                        break;
+                    }
                     if (B!="print")
                     {
-                        clientObject1.Get("stop");
+                        clientObject.Get("stop");
                     }
                 }
                 else
